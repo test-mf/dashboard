e3cf3b53089089fa0203c30bd29b16f0aa4897d6
d0kur0 <dev.dokuro@gmail.com>
2022-06-05 03:44:55 +0300

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="user" v-if="userInfo">Hello, {{ userInfo.firstName }}</div>
+	<div class="user" v-if="!userInfo">LOADING USER INFO</div>
+</template>
+
+<script>
+import { getUserInfo, isUserLogin } from "@test-mf/api";
+
+export default {
+	name: "DashBoard",
+	data() {
+		return {
+			userInfo: null,
+		};
+	},
+	mounted() {
+		isUserLogin() || history.pushState(null, null, "#/signIn");
+		getUserInfo().then(info => (this.userInfo = info));
+	},
+};
+</script>
+
+<style>
+.user {
+	display: flex;
+	justify-content: center;
+	font-size: 2em;
+}
+</style>
